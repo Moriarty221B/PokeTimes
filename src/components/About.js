8a143ee73f8ab571247a4d3ee7cc53220f8edd79
Moriarty221B <41786041+Moriarty221B@users.noreply.github.com>
2019-12-04 11:44:15 +0200

    Add files via upload
    Initial basic template

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About =()=>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem dolore ipsum dolorum autem quibusdam neque, architecto unde magni nesciunt illum itaque atque velit tenetur et asperiores at dicta error laborum!</p>
+        </div>
+    )
+}
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home =()=>{
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem dolore ipsum dolorum autem quibusdam neque, architecto unde magni nesciunt illum itaque atque velit tenetur et asperiores at dicta error laborum!</p>
+        </div>
+    )
+}
+export default Home
